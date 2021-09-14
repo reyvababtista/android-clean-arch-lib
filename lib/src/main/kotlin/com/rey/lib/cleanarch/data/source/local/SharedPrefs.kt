1b3039de1b15737a4209d8289d2c0190ed5e9c69
Reyva Babtista <reyvababtista16@gmail.com>
2021-09-14 14:50:16 +0700

    refactor: add getPref function

--- lib/src/main/kotlin/com/rey/lib/cleanarch/data/source/local/SharedPrefs.kt
+++ lib/src/main/kotlin/com/rey/lib/cleanarch/data/source/local/SharedPrefs.kt
@@ -1,11 +1,11 @@
 package com.rey.lib.cleanarch.data.source.local
 
 import android.content.SharedPreferences
-import com.rey.lib.cleanarch.data.repository.source.local.ISecureSharedPreferences
+import com.rey.lib.cleanarch.data.repository.source.local.ISharedPrefs
 import com.rey.lib.cleanarch.domain.dto.Result
 import com.rey.lib.cleanarch.domain.dto.suspendTryCatch
 
-class SecureSharedPreferences(private val pref: SharedPreferences) : ISecureSharedPreferences {
+class SharedPrefs(private val pref: SharedPreferences) : ISharedPrefs {
     override suspend fun set(key: String, value: Any): Result<Unit> = suspendTryCatch {
         with(pref.edit()) {
             when (value) {
@@ -21,4 +21,8 @@ class SecureSharedPreferences(private val pref: SharedPreferences) : ISecureShar
 
         Result.Success(Unit)
     }
+
+    override suspend fun getPref(): Result<SharedPreferences> =
+        suspendTryCatch { Result.Success(pref) }
+
 }
