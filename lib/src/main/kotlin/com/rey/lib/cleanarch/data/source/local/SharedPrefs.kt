cfe28ae4e0946a8d5a916a23ff502044a4244871
Reyva Babtista <reyvababtista16@gmail.com>
2021-09-14 14:52:31 +0700

    refactor: change function signatures

--- lib/src/main/kotlin/com/rey/lib/cleanarch/data/source/local/SharedPrefs.kt
+++ lib/src/main/kotlin/com/rey/lib/cleanarch/data/source/local/SharedPrefs.kt
@@ -2,27 +2,19 @@ package com.rey.lib.cleanarch.data.source.local
 
 import android.content.SharedPreferences
 import com.rey.lib.cleanarch.data.repository.source.local.ISharedPrefs
-import com.rey.lib.cleanarch.domain.dto.Result
-import com.rey.lib.cleanarch.domain.dto.suspendTryCatch
 
 class SharedPrefs(private val pref: SharedPreferences) : ISharedPrefs {
-    override suspend fun set(key: String, value: Any): Result<Unit> = suspendTryCatch {
-        with(pref.edit()) {
-            when (value) {
-                is Boolean -> putBoolean(key, value)
-                is Int -> putInt(key, value)
-                is Long -> putLong(key, value)
-                is String -> putString(key, value)
-                is Float -> putFloat(key, value)
-                else -> throw IllegalArgumentException("unsupported data type ${value.javaClass}")
-            }
-            apply()
+    override suspend fun set(key: String, value: Any): Unit = with(pref.edit()) {
+        when (value) {
+            is Boolean -> putBoolean(key, value)
+            is Int -> putInt(key, value)
+            is Long -> putLong(key, value)
+            is String -> putString(key, value)
+            is Float -> putFloat(key, value)
+            else -> throw IllegalArgumentException("unsupported data type ${value.javaClass}")
         }
-
-        Result.Success(Unit)
+        apply()
     }
 
-    override suspend fun getPref(): Result<SharedPreferences> =
-        suspendTryCatch { Result.Success(pref) }
-
+    override suspend fun getPref(): SharedPreferences = pref
 }
